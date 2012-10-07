b51fa7f5666c588bd9e2bdbef0333bd46204f065
Alex Mullans <alex@alexmullans.com>
2012-10-07 17:21:40 -0400

    Add Lab1.

---
+++ MyTVCompanion/TvdbAdapterConsoleTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TvdbLib;
+using TvdbLib.Cache;
+
+namespace TvdbAdapterConsoleTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tvdbHandler = new TvdbHandler(new XmlCacheProvider("%temp%"),"49FF3082EF06CF50");
+            tvdbHandler.InitCache();
+            var searchResults = tvdbHandler.SearchSeries("Big Bang");
+
+            Console.ReadLine();
+        }
+    }
+}
