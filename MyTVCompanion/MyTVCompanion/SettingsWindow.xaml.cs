beb3e21f5ffed3664a93427af911350caaff583e
Alex Mullans <alex@alexmullans.com>
2012-10-09 10:52:39 -0400

    Initial Lab2 TVDB work.

--- MyTVCompanion/MyTVCompanion/SettingsWindow.xaml.cs
+++ MyTVCompanion/MyTVCompanion/SettingsWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TvdbLib;
+using TvdbLib.Cache;
+using TvdbLib.Data;
 
 namespace MyTVCompanion
 {
@@ -19,9 +22,28 @@ namespace MyTVCompanion
     /// </summary>
     public partial class SettingsWindow : Window
     {
+        private TvdbHandler _tvdbHandler;
+
         public SettingsWindow()
         {
             InitializeComponent();
+            _tvdbHandler = new TvdbHandler(new XmlCacheProvider("%temp%"), "49FF3082EF06CF50");
+            _tvdbHandler.InitCache();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            var results = _tvdbHandler.SearchSeries(SearchBox.Text);
+            SearchResults.ItemsSource= results;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (SearchResults.SelectedIndex == -1) return;
+            var watchedShows = new List<TvdbData>();
+            //SearchResults.ItemsSource.Cast<TvdbSearchResult>().Where(());
+            //watchedShows.Add(_tvdbHandler.GetSeries(,TvdbLanguage.DefaultLanguage, false,false,false));
         }
     }
 }
